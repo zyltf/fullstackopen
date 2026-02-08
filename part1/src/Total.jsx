8eb348a046712ad6134f84f823649313ce5475b0
zyltf <zolkyar@gmail.com>
2026-02-08 01:11:42 +0100

    exercise 1.3 commit

--- part1/src/Total.jsx
+++ part1/src/Total.jsx
@@ -1,7 +1,7 @@
 const Total = (props) => {
     return (
         <>
-            <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+            <p>Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
         </>
     )
 }
