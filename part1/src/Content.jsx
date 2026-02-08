1e634984453be806c98960113fc0283d9878934c
zyltf <zolkyar@gmail.com>
2026-02-08 01:19:06 +0100

    exercise 1.4 commit

--- part1/src/Content.jsx
+++ part1/src/Content.jsx
@@ -2,9 +2,9 @@ import Part from './Part'
 const Content = (props) => {
     return (
         <>
-            <Part part={props.part1.name} exercises={props.part1.exercises}/>
-            <Part part={props.part2.name} exercises={props.part2.exercises}/>
-            <Part part={props.part3.name} exercises={props.part3.exercises}/>
+            <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
+            <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
+            <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
         </>
     )
 }
