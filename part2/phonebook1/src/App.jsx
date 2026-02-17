2951309b14434766374fed881165623d7c756d5f
00zkr <zakariarhendour@gmail.com>
2026-02-17 02:33:33 +0000

    exercise 2.11 commit

--- part2/phonebook1/src/App.jsx
+++ part2/phonebook1/src/App.jsx
@@ -1,19 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './Filter'
 import PersonForm from './PersonForm'
 import Persons from './Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log("promise fulfilled")
+        setPersons(response.data)
+      }
+
+      )
+  }
+  useEffect(hook, [])
 
   const personsToShow = showAll 
   ? persons 
